006318fca5daff2e6ce7806a6aee0193c3da68d5
Peter Lyons <pete@peterlyons.com>
2012-11-05 23:12:07 -0700

    Fix up symlink detection

--- makitso.sh
+++ makitso.sh
@@ -4,8 +4,9 @@
 #This enables a developer to just clone the git repo, read the README.md
 #and start issuing "do" commands without futzing with manual prerequisite
 #installation too much
-
-cd $(dirname "${0}")
+THIS_FILE=$(readlink "${0}") || /usr/bin/true
+[ -z "${THIS_FILE}" ] && THIS_FILE="${0}"
+cd $(dirname "${THIS_FILE}")
 PYTHON_ROOT="../python"
 FAB_PATH="${PYTHON_ROOT}/bin/fab"
 
