5056163ef04b11677f943474fec744ce410d0caa
Peter Lyons <pete@peterlyons.com>
2012-11-06 22:11:16 -0700

    tweaks to node install arg processing

--- scripts/node/install_node.sh
+++ scripts/node/install_node.sh
@@ -3,10 +3,11 @@ NODE_ROOT="${1}"
 if [ -z "${NODE_ROOT}" ]; then
   NODE_ROOT=$(dirname "${0}/../../../node")
 fi
+shift
 
 install_node() {
     local PREFIX=${1-node}
-    local VERSION=${2-0.8.8}
+    local VERSION=${2-0.8.14}
     local PLATFORM=$(uname | tr A-Z a-z)
     local ARCH=x64
     case $(uname -p) in
@@ -24,5 +25,6 @@ if [ -x "${NODE_ROOT}/bin/node" ]; then
     #all good
     exit 0
 fi
+
 echo "node not installed in ${NODE_ROOT}, installing..."
-install_node "${NODE_ROOT}"
+install_node "${NODE_ROOT}" "${@}"
